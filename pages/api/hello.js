98832c27fa8e23433c1e412c149ab7a064a45504
kosuke.sakai <kosuke.sakai@bridge-net.com>
2022-05-21 23:09:05 +0900

    test

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+// APIを使う際の記述
 export default function handler(req, res) {
+  // status 200：正常なレスポンス
   res.status(200).json({ name: 'John Doe' })
 }
